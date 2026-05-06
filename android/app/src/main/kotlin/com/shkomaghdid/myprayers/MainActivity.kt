469cafb01208f5603ea4c572d38aaaee2b863314
ShkoMaghdid <Shkoma.ranya@gmail.com>
2026-05-06 22:53:50 +0300

    changes

--- android/app/src/main/kotlin/com/shkomaghdid/myprayers/MainActivity.kt
+++ android/app/src/main/kotlin/com/shkomaghdid/myprayers/MainActivity.kt
@@ -1,5 +1,14 @@
 package com.shkomaghdid.myprayers
 
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // Install the Android 12+ splash screen before super.onCreate() so the
+        // system can transition smoothly from LaunchTheme.Splash into Flutter.
+        installSplashScreen()
+        super.onCreate(savedInstanceState)
+    }
+}

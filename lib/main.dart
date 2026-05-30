4e30cecd0c47d6ab33f33bde173855325ae8df19
ShkoMaghdid <Shkoma.ranya@gmail.com>
2026-05-30 05:09:11 +0300

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_displaymode/flutter_displaymode.dart';
@@ -21,10 +23,15 @@ Future<void> main() async {
     overlays: SystemUiOverlay.values,
   );
 
+  const launchIcon = AssetImage('assets/widget/launch_icon.png');
+  final iconReady = _precacheLaunchIcon(launchIcon);
+
   final settings = await SettingsNotifier.create();
   final favorites = await FavoritesNotifier.create();
   await MuslimDb.instance.open();
   await NotificationService.instance.init();
+
+  await iconReady;
 
   runApp(
     ProviderScope(
@@ -60,6 +67,26 @@ Future<void> main() async {
   });
 
   WidgetsBinding.instance.addObserver(_LifecycleHook(tryReschedule));
+}
+
+Future<void> _precacheLaunchIcon(AssetImage provider) {
+  final completer = Completer<void>();
+  final stream = provider.resolve(ImageConfiguration.empty);
+  late final ImageStreamListener listener;
+  void done() {
+    stream.removeListener(listener);
+    if (!completer.isCompleted) completer.complete();
+  }
+
+  listener = ImageStreamListener(
+    (_, _) => done(),
+    onError: (_, _) => done(),
+  );
+  stream.addListener(listener);
+  return completer.future.timeout(
+    const Duration(milliseconds: 600),
+    onTimeout: () {},
+  );
 }
 
 class _LifecycleHook with WidgetsBindingObserver {
